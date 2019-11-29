78f5437d9e271dae83da657d9ab2def185bcd200
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-11-30 07:24:42 +0800

    Add user validates presence

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,9 @@
 class Author < ApplicationRecord
   has_secure_password
   has_many :articles
+
+  # validations
+  validates :full_name, presence: true
+  validates :email, presence: true
+  validates :password, :presence: true
 end
