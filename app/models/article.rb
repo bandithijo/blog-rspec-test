f49af899682d6365cda3aa7c7b2b458a14d534c8
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-11-30 07:11:37 +0800

    Rename user id column to author id on articles table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :author
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_29_225937) do
+ActiveRecord::Schema.define(version: 2019_11_29_230720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_11_29_225937) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "post"
-    t.integer "user_id"
+    t.integer "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
