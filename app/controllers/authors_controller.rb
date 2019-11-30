fe7b86ad238487ed94b99c62743fb925d2f884cb
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-11-30 09:54:06 +0800

    Add Authors destroy action

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -33,6 +33,12 @@ class AuthorsController < ApplicationController
     end
   end
 
+  def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+    redirect_to authors_path
+  end
+
   private
 
   def author_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :authors, only: %w[index show new create edit update]
+  resources :authors, only: %w[index show new create edit update destroy]
 end
