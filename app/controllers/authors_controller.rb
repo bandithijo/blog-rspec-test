57cf0526f6f83c0c964a5736c9806c53ab75c87f
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-11-30 09:49:54 +0800

    Add Authors new create action

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,28 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Author.all
+    @authors = Author.includes(:articles).all
   end
 
   def show
     @author = Author.find(params[:id])
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:full_name, :email, :password)
   end
 end
