2e063a6fea72f64ba3ccaf24f393d9cc8e7f958b
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-11-30 10:28:02 +0800

    Add Articles destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,6 +33,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def article_params
