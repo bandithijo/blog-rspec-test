f7f1b36e740a38192594052c385171a580f4f5c0
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2019-11-30 09:15:03 +0800

    Add more detail of validation length tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,9 @@ class Article < ApplicationRecord
   belongs_to :author
 
   # validations
-  validates :title, presence: true
-  validates :post, presence: true
+  validates :title, presence: true,
+                    length: {minimum: 5, maximum: 30}
+  validates :post, presence: true,
+                   length: {minimum: 5}
   validates :author_id, numericality: {only_integer: true}
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -28,16 +28,45 @@ RSpec.describe Article, type: :model do
       ).save
       expect(article).to eq(false)
     end
-    it 'Should save successfully'
+
+    it 'Should save successfully' do
+      article = Article.new(
+        title: Faker::Team.name,
+        post: Faker::Hipster.paragraphs.map! { |w| "#{w}" }.join,
+        author_id: 1
+      ).save
+      expect(article).to eq(true)
+    end
   end
 
   context 'Validation Length Tests' do
-    it 'Ensures title character  length, not less than 5'
-    it 'Ensures title character length, not more than 30'
-    it 'Ensures post character length, not less than 5'
+    it 'Ensures title character  length, not less than 5' do
+      article = Article.new(
+        title: 'ban',
+        post: Faker::Hipster.paragraphs.map! { |w| "#{w}" }.join,
+        author_id: 1
+      ).save
+      expect(article).to eq(false)
+    end
+
+    it 'Ensures title character length, not more than 30' do
+      article = Article.new(
+        title: 'bandithijobandithijobandithijobandithijo',
+        post: Faker::Hipster.paragraphs.map! { |w| "#{w}" }.join,
+        author_id: 1
+      ).save
+      expect(article).to eq(false)
+    end
+
+    it 'Ensures post character length, not less than 5' do
+      article = Article.new(
+        title: Faker::Team.name,
+        post: 'ban',
+        author_id: 1
+      ).save
+      expect(article).to eq(false)
+    end
   end
 
-  context 'Data Type Tests' do
-    it 'Ensures Author ID is numericality'
-  end
+  context 'Data Type Tests'
 end
